4fd9412f2f30000bb12ee9dd6e8c40610bf42ef1
Jungzhang <zhanggen.jung@gmail.com>
2015-08-05 09:36:37 +0800

    select函数练习

---
+++ 第十一章/select.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <time.h>
+
+void display_time(const char *string)
+{
+	int seconds;
+	seconds = time((time_t *)NULL);
+	printf("%s%d\n",string,seconds);
+}
+
+int main(void)
+{
+	fd_set readfds;
+	struct timeval timeout;
+	int ret;
+
+	FD_ZERO(&readfds);	//开始使用一个描述符集合前将其清空
+	FD_SET(0,&readfds);
+
+	timeout.tv_sec = 10;	//秒数为10秒
+	timeout.tv_usec = 0;	//微秒数为0
+
+	while(1)
+	{
+		display_time("select之前:");
+		ret = select(1,&readfds,NULL,NULL,&timeout);
+		display_time("select之后：");
+
+		switch(ret)
+		{
+			case 0:
+				printf("没有数据\n");
+				exit(0);
+			case -1:
+				perror("select error：");
+				exit(-1);
+			default:
+				getchar();
+				printf("有数据\n");
+		}
+	}
+
+	return EXIT_SUCCESS;
+}
