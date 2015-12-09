9aa031a6ef7144705e28224462df9a916b49a728
Jungzhang <zhanggen.jung@gmail.com>
2015-12-09 22:07:45 +0800

    add

--- 数据结构/Graph_List.c
+++ 数据结构/Graph_List.c
@@ -7,6 +7,8 @@
  ************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
+#include "GraphStack.h"
+#include "GraphQueue.h"
 
 #define MAXSIZE 88
 
@@ -37,7 +39,7 @@ GRAPH *InitGraph(void)
     graph = (GRAPH *)malloc(sizeof(GRAPH));
     graph->len = 0;
     
-    printf("请输入顶点数据：");
+//    printf("请输入顶点数据：");
     data = getchar();
 
     while (data != '\n'){
@@ -88,7 +90,7 @@ void CreatGraph(GRAPH *graph)
     side *pNew;
 
     while(1){
-        printf("请输入图的关系集合(start end weight,如：a b 30)输入#结束输入：");
+ //       printf("请输入图的关系集合(start end weight,如：a b 30)输入#结束输入：");
         scanf("%c", &start);
         if (start == '#'){
             break;
@@ -130,10 +132,9 @@ int AdjVex(GRAPH *graph, int ver, int visit)
 
 
 //DFS(递归)
-void DFS(GRAPH *graph, int ver, int visited[])
+void DFS_R(GRAPH *graph, int ver, int visited[])
 {
     int visit = ver;      //要访问的结点下标
-    
     printf("%c", graph->v[visit].data);
     visited[visit] = 1;
     visit = AdjVex(graph, ver, visit);      //取出ver的下一个邻接点
@@ -141,10 +142,40 @@ void DFS(GRAPH *graph, int ver, int visited[])
     while (visit != -1){
         
         if (!visited[visit]){           //如果没有被访问过
-            DFS(graph, visit, visited);          //DFS该结点
+            DFS_R(graph, visit, visited);          //DFS该结点
         }
         
         visit = AdjVex(graph, ver, visit);    //取出ver的下一个邻接点
+    }
+}
+
+//DFS(非递归)
+void DFS(GRAPH *graph, int ver, int visited[])
+{
+    int visit = ver;
+    STACK *stack = InitStack();
+    
+    printf("%c", graph->v[visit].data);
+    visited[visit] = 1;
+    PushStack(stack, visit);
+
+    while (!IsEmpty(stack)) {
+        visit = AdjVex(graph, ver, visit);
+        while (visit != -1) {
+            if (!visited[visit]) {
+                printf("%c", graph->v[visit].data);
+                visited[visit] = 1;
+                PushStack(stack, visit);
+                ver = visit;  //将出度结点更改为新结点
+                visit = AdjVex(graph, ver, visit);  //找到新出度结点的下一个邻接点
+            } else {
+                visit = AdjVex(graph, ver, visit);
+            }
+        }
+        PopStack(stack, &visit);
+        if (!IsEmpty(stack)){
+            GetTop(stack, &ver);
+        }
     }
 }
 
@@ -153,25 +184,126 @@ void TraverseDFS(GRAPH *graph)
 {
     int i;
     int visited[MAXSIZE] = {0,0,0};
+
+    printf("递归：");
+
     for (i = 0; i < graph->len; i++){
         if (!visited[i]){
+            DFS_R(graph, i, visited);
+        }
+    }
+
+    printf("\n\n");
+
+    for (i = 0; i < MAXSIZE && i <= graph->len; i++){
+        visited[i] = 0;
+    }
+
+    printf("非递归：");
+
+    for (i = 0; i < graph->len; i++){
+        if (!visited[i]) {
             DFS(graph, i, visited);
         }
     }
+
     printf("\n");
 }
 
 //BFS
+void BFS(GRAPH *graph, int ver, int visited[])
+{
+    int visit = ver;
+    Queue *q = InitQueue();
+    
+    printf("%c", graph->v[ver].data);
+    visited[visit] = 1;
+    InQueue(q, visit);
+    
+    while (!QIsEmpty(q)) {
+        
+        OutQueue(q, &ver);
+        visit = ver;
+        visit = AdjVex(graph, ver, visit);
+        
+        while (visit != -1) {
+            
+            if (!visited[visit]) {
+                printf("%c", graph->v[visit].data);
+                visited[visit] = 1;
+                InQueue(q, visit);
+            }
+            
+            visit = AdjVex(graph, ver, visit);
+        }
+    }
+}
 
 //广搜遍历
+void TraverseBFS(GRAPH *graph)
+{
+    int visited[MAXSIZE] = {0, 0, 0};
+    int i;
+
+    printf("广度优先搜索：");
+
+    for (i = 0; i < graph->len; i++) {
+        
+        if (!visited[i]) {
+            BFS(graph, i, visited);
+        }       
+    }
+
+    printf("\n");
+
+}
 
 //计算图的度
+void degree(GRAPH *graph, int ver, int *in_degree, int *out_degree, int *total)
+{
+    int i;
+    side *pTemp;
+
+    *in_degree = 0;
+    *out_degree = 0;
+    *total = 0;
+
+    for (i = 0; i < graph->len; i++) {
+        
+        pTemp = graph->v[i].pFirst->pNext;
+
+        while (pTemp) {
+            
+            if (i == ver) {
+                ++(*out_degree);
+            }
+            
+            if (pTemp->pos == ver) {
+                ++(*in_degree);
+            }
+            pTemp = pTemp->pNext;
+        }
+    }
+
+    (*total) = (*out_degree) + (*in_degree);
+}
 
 int main(int argc, char *argv[])
 {
+    int i, in_degree, out_degree, total;
     GRAPH *graph = InitGraph();
     CreatGraph(graph);
+    printf("\n");
     TraverseDFS(graph);
+    printf("\n");
+    TraverseBFS(graph);
+
+    printf("\n%10s%10s%10s%10s\n", "顶点", "入度", "出度", "总度");
+
+    for (i = 0; i < graph->len; i++) {
+        degree(graph, i, &in_degree, &out_degree, &total);
+        printf("%6c%9d%8d%8d\n", graph->v[i].data, in_degree, out_degree, total);
+    }
     
     return 0;
 }
