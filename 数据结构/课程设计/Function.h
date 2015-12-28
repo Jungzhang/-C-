607c2d8b4f7ae2c3e9e918f2725e179e33deebf6
Jungzhang <zhanggen.jung@gmail.com>
2015-12-28 11:47:31 +0800

    add

---
+++ 数据结构/课程设计/Function.h
@@ -0,0 +1,17 @@
+/*************************************************************************
+	> File Name: Function.h
+	> Author: Jung
+	> Mail: jungzhang@xiyoulinux.org  or  zhanggen.jung@gmail.com
+	> Created Time: 2015年12月27日 星期日 14时35分14秒
+	> Description:
+ ************************************************************************/
+#ifndef _FUNCTION_H
+#define _FUNCTION_H
+
+//解压缩函数
+int Decompression(char *SourceFileName, char *TarFileName);
+
+//压缩函数
+int Compression(char SourceFileName[], char TarFileName[]);
+
+#endif
